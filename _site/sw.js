f8f0e198d71d1721528e0d0961ce40e08e74dbf3
asantoshka <asantoshka@outlook.com>
2021-05-18 01:13:57 +0530

    Added about page

--- _site/sw.js
+++ _site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202105180110",
+        "/css/pixyll.css?202105180113",
         "/"
       ]);
     })
